8c9c86a0170180941519cae629210736d50ed2a8
rakesh <vgrakesh527@gmail.com>
2025-01-07 22:06:15 +0530

    initial commit

---
+++ src/test/java/com/rak/agora/AgoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rak.agora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
